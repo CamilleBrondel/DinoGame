f00fdc87b6701b9c3cad05cacde0c8a6c23f4d28
Camille Brondel <brondelcamille@gmail.com>
2021-03-25 16:02:23 +0100

    BasicScene Start

---
+++ DinoGame/Assets/Assets/Scripts/Agents/BasicAgentAi.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAgentAi : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 3f;
+    [SerializeField] private float _rotSpeed = 3f;
+
+    [SerializeField] private bool _isWandering =  false;
+    [SerializeField] private bool _isRotatingLeft =  false;
+    [SerializeField] private bool _isRotatingRight =  false;
+    [SerializeField] private bool _isWalking =  false;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
